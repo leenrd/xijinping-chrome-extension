26ad31033a003657b37868af4ee569c62a79fdbe
Leenard <lolleenard0@gmail.com>
2024-05-15 16:58:54 +0800

    feat: ☠

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
-const replacementImageURL = "./xi.webp";
+// URL of the replacement image
+const replacementImageURL = chrome.runtime.getURL("xi.webp");
 
+// Function to replace all images
 function replaceImages() {
   const images = document.querySelectorAll("img");
   images.forEach((img) => {
     img.src = replacementImageURL;
+    img.srcset = replacementImageURL; // Handle srcset attribute for responsive images
   });
 }
 
+// Run the replaceImages function when the content script is loaded
 replaceImages();
